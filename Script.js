d3b778557ed1691623c07a5e121ac4ba1ce41629
Saptak Majumdar <106856957+Coder-saptak291@users.noreply.github.com>
2022-12-28 17:02:37 +0530

    Add files via upload

--- Script.js
+++ Script.js
@@ -2,12 +2,13 @@ let audioElement = new Audio("1.mp3");
 let audioElement1 = new Audio("2.mp3");
 let audioElement2 = new Audio("3.mp3");
 let audioElement3 = new Audio("4.mp3");
-
 let audioElement4 = new Audio("5.mp3");
 let audioElement5 = new Audio("6.mp3");
 let audioElement6 = new Audio("7.mp3");
 let audioElement7 = new Audio("8.mp3");
 let audioElement8 = new Audio("9.mp3");
+let audioElement9 = new Audio("10.mp3");
+let audioElement10 = new Audio("11.mp3");
 
 let play = document.getElementById("btn");
 let play1 = document.getElementById("btn1");
@@ -18,27 +19,34 @@ let play5 = document.getElementById("btn5");
 let play6 = document.getElementById("btn6");
 let play7 = document.getElementById("btn7");
 let play8 = document.getElementById("btn8");
+let play9 = document.getElementById("btn9");
+let play10 = document.getElementById("btn10");
 play.addEventListener("click", () => {
   if (audioElement.paused) {
     audioElement.play();
     play.innerHTML = "Pause";
+
     audioElement1.pause();
     audioElement2.pause();
+    audioElement3.pause();
+    audioElement4.pause();
+    audioElement5.pause();
+    audioElement6.pause();
+    audioElement7.pause();
+    audioElement8.pause();
+    audioElement9.pause();
+    audioElement10.pause();
 
     play1.innerHTML = "Play";
     play2.innerHTML = "Play";
-    audioElement3.pause();
-    audioElement4.pause();
     play3.innerHTML = "Play";
     play4.innerHTML = "Play";
-    audioElement5.pause();
-    audioElement6.pause();
     play5.innerHTML = "Play";
     play6.innerHTML = "Play";
-    audioElement7.pause();
     play7.innerHTML = "Play";
-    audioElement8.pause();
     play8.innerHTML = "Play";
+    play9.innerHTML = "Play";
+    play10.innerHTML = "Play";
   } else {
     audioElement.pause();
     play.innerHTML = "Play";
@@ -48,22 +56,29 @@ play1.addEventListener("click", () => {
   if (audioElement1.paused) {
     audioElement1.play();
     play1.innerHTML = "Pause";
+    
     audioElement.pause();
     audioElement2.pause();
     audioElement3.pause();
     audioElement4.pause();
-    play3.innerHTML = "Play";
-    play4.innerHTML = "Play";
-    play.innerHTML = "Play";
-    play2.innerHTML = "Play";
     audioElement5.pause();
     audioElement6.pause();
     audioElement7.pause();
-    play7.innerHTML = "Play";
     audioElement8.pause();
-    play8.innerHTML = "Play";
+    audioElement9.pause();
+    audioElement10.pause();
+    
+    
+    play.innerHTML = "Play";
+    play2.innerHTML = "Play";
+    play3.innerHTML = "Play";
+    play4.innerHTML = "Play";
     play5.innerHTML = "Play";
     play6.innerHTML = "Play";
+    play7.innerHTML = "Play";
+    play8.innerHTML = "Play";
+    play9.innerHTML = "Play";
+    play10.innerHTML = "Play"
   } else {
     audioElement1.pause();
     play1.innerHTML = "Play";
@@ -72,23 +87,29 @@ play1.addEventListener("click", () => {
 play2.addEventListener("click", () => {
   if (audioElement2.paused) {
     audioElement2.play();
-    audioElement7.pause();
-    play7.innerHTML = "Play";
-    audioElement8.pause();
-    play8.innerHTML = "Play";
     play2.innerHTML = "Pause";
+    
     audioElement.pause();
     audioElement1.pause();
     audioElement3.pause();
     audioElement4.pause();
-    play3.innerHTML = "Play";
-    play4.innerHTML = "Play";
-    play.innerHTML = "Play";
-    play1.innerHTML = "Play";
     audioElement5.pause();
     audioElement6.pause();
+    audioElement7.pause();
+    audioElement8.pause();
+    audioElement9.pause();
+    audioElement10.pause();
+
+    play.innerHTML = "Play";
+    play1.innerHTML = "Play";
+    play3.innerHTML = "Play";
+    play4.innerHTML = "Play";
     play5.innerHTML = "Play";
     play6.innerHTML = "Play";
+    play7.innerHTML = "Play";
+    play8.innerHTML = "Play";
+    play9.innerHTML = "Play";
+    play10.innerHTML = "Play"
   } else {
     audioElement2.pause();
     play2.innerHTML = "Play";
@@ -96,24 +117,30 @@ play2.addEventListener("click", () => {
 });
 play3.addEventListener("click", () => {
   if (audioElement3.paused) {
-    audioElement7.pause();
-    play7.innerHTML = "Play";
-    audioElement8.pause();
-    play8.innerHTML = "Play";
     audioElement3.play();
     play3.innerHTML = "Pause";
+
+    audioElement.pause();
     audioElement1.pause();
     audioElement2.pause();
-    audioElement.pause();
     audioElement4.pause();
-    audioElement6.pause();
     audioElement5.pause();
+    audioElement6.pause();
+    audioElement7.pause();
+    audioElement8.pause();
+    audioElement9.pause();
+    audioElement10.pause();
+
+    play.innerHTML = "Play";
     play1.innerHTML = "Play";
     play2.innerHTML = "Play";
     play4.innerHTML = "Play";
     play5.innerHTML = "Play";
     play6.innerHTML = "Play";
-    play.innerHTML = "Play";
+    play7.innerHTML = "Play";
+    play8.innerHTML = "Play";
+    play9.innerHTML = "Play";
+    play10.innerHTML = "Play";
   } else {
     audioElement3.pause();
     play3.innerHTML = "Play";
@@ -121,24 +148,30 @@ play3.addEventListener("click", () => {
 });
 play4.addEventListener("click", () => {
   if (audioElement4.paused) {
-    audioElement7.pause();
-    play7.innerHTML = "Play";
-    audioElement8.pause();
-    play8.innerHTML = "Play";
     audioElement4.play();
     play4.innerHTML = "Pause";
+
+    audioElement.pause();
     audioElement1.pause();
     audioElement2.pause();
     audioElement3.pause();
-    audioElement.pause();
+    audioElement5.pause();
+    audioElement6.pause();
+    audioElement7.pause();
+    audioElement8.pause();
+    audioElement9.pause();
+    audioElement10.pause();
+
+    play.innerHTML = "Play";
     play1.innerHTML = "Play";
     play2.innerHTML = "Play";
     play3.innerHTML = "Play";
-    play.innerHTML = "Play";
-    audioElement5.pause();
-    audioElement6.pause();
     play5.innerHTML = "Play";
     play6.innerHTML = "Play";
+    play7.innerHTML = "Play";
+    play8.innerHTML = "Play";
+    play9.innerHTML = "Play";
+    play10.innerHTML = "Play";
   } else {
     audioElement4.pause();
     play4.innerHTML = "Play";
@@ -146,26 +179,30 @@ play4.addEventListener("click", () => {
 });
 play5.addEventListener("click", () => {
   if (audioElement5.paused) {
-    audioElement7.pause();
-    play7.innerHTML = "Play";
-    audioElement8.pause();
-    play8.innerHTML = "Play";
     audioElement5.play();
     play5.innerHTML = "Pause";
+    
+    audioElement.pause();
     audioElement1.pause();
     audioElement2.pause();
-
     audioElement3.pause();
     audioElement4.pause();
     audioElement6.pause();
-
-    audioElement.pause();
+    audioElement7.pause();
+    audioElement8.pause();
+    audioElement9.pause();
+    audioElement10.pause();
+    
+    play.innerHTML = "Play";
     play1.innerHTML = "Play";
     play2.innerHTML = "Play";
     play3.innerHTML = "Play";
     play4.innerHTML = "Play";
     play6.innerHTML = "Play";
-    play.innerHTML = "Play";
+    play7.innerHTML = "Play";
+    play8.innerHTML = "Play";
+    play9.innerHTML = "Play";
+    play10.innerHTML = "Play";
   } else {
     audioElement5.pause();
     play5.innerHTML = "Play";
@@ -173,26 +210,30 @@ play5.addEventListener("click", () => {
 });
 play6.addEventListener("click", () => {
   if (audioElement6.paused) {
-    audioElement7.pause();
-    play7.innerHTML = "Play";
-    audioElement8.pause();
-    play8.innerHTML = "Play";
     audioElement6.play();
     play6.innerHTML = "Pause";
-    audioElement1.pause();
-    audioElement2.pause();
-    audioElement5.pause();
-    audioElement3.pause();
-    audioElement4.pause();
-    audioElement.pause();
 
     audioElement.pause();
+    audioElement1.pause();
+    audioElement2.pause();
+    audioElement3.pause();
+    audioElement4.pause();
+    audioElement5.pause();
+    audioElement7.pause();
+    audioElement8.pause();
+    audioElement9.pause();
+    audioElement10.pause();
+
+    play.innerHTML = "Play";
     play1.innerHTML = "Play";
     play2.innerHTML = "Play";
     play3.innerHTML = "Play";
     play4.innerHTML = "Play";
     play5.innerHTML = "Play";
-    play.innerHTML = "Play";
+    play7.innerHTML = "Play";
+    play8.innerHTML = "Play";
+    play9.innerHTML = "Play";
+    play10.innerHTML = "Play";
   } else {
     audioElement6.pause();
     play6.innerHTML = "Play";
@@ -202,21 +243,27 @@ play7.addEventListener("click", () => {
   if (audioElement7.paused) {
     audioElement7.play();
     play7.innerHTML = "Pause";
+    audioElement.pause();
     audioElement1.pause();
     audioElement2.pause();
-
-    play1.innerHTML = "Play";
-    play2.innerHTML = "Play";
     audioElement3.pause();
     audioElement4.pause();
-    play3.innerHTML = "Play";
-    play4.innerHTML = "Play";
     audioElement5.pause();
     audioElement6.pause();
+    audioElement8.pause();
+    audioElement9.pause();
+    audioElement10.pause();
+    
+    play.innerHTML = "Play";
+    play1.innerHTML = "Play";
+    play2.innerHTML = "Play";
+    play3.innerHTML = "Play";
+    play4.innerHTML = "Play";
     play5.innerHTML = "Play";
     play6.innerHTML = "Play";
-    audioElement8.pause();
     play8.innerHTML = "Play";
+    play9.innerHTML = "Play";
+    play10.innerHTML = "Play";
   } else {
     audioElement7.pause();
     play7.innerHTML = "Play";
@@ -226,27 +273,95 @@ play8.addEventListener("click", () => {
   if (audioElement8.paused) {
     audioElement8.play();
     play8.innerHTML = "Pause";
+
+    audioElement.pause();
     audioElement1.pause();
     audioElement2.pause();
+    audioElement3.pause();
+    audioElement4.pause();
+    audioElement5.pause();
+    audioElement6.pause();
+    audioElement7.pause();
+    audioElement9.pause();
+    audioElement10.pause();
 
     play1.innerHTML = "Play";
     play2.innerHTML = "Play";
-    audioElement3.pause();
-    audioElement4.pause();
     play3.innerHTML = "Play";
     play4.innerHTML = "Play";
-    audioElement5.pause();
-    audioElement6.pause();
     play5.innerHTML = "Play";
     play6.innerHTML = "Play";
-    audioElement7.pause();
     play7.innerHTML = "Play";
+    play9.innerHTML = "Play";
+    play10.innerHTML = "Play"
   } else {
-    audioElement.pause();
+    audioElement8.pause();
     play8.innerHTML = "Play";
   }
 });
-// audioElement7.pause();
-// play7.innerHTML = "Play";
-// audioElement8.pause();
-// play8.innerHTML = "Play";
+
+play9.addEventListener("click", () => {
+  if (audioElement9.paused) {
+    audioElement9.play();
+    play9.innerHTML = "Pause";
+
+    audioElement.pause();
+    audioElement1.pause();
+    audioElement2.pause();
+    audioElement3.pause();
+    audioElement4.pause();
+    audioElement5.pause();
+    audioElement6.pause();
+    audioElement7.pause();
+    audioElement8.pause();
+    audioElement10.pause();
+
+    play1.innerHTML = "Play";
+    play2.innerHTML = "Play";
+    play3.innerHTML = "Play";
+    play4.innerHTML = "Play";
+    play5.innerHTML = "Play";
+    play6.innerHTML = "Play";
+    play7.innerHTML = "Play";
+    play8.innerHTML = "Play";
+    play10.innerHTML = "Play"
+  }
+   else {
+    audioElement9.pause();
+    play9.innerHTML = "Play";
+  }
+});
+
+play10.addEventListener("click", () => {
+  if (audioElement10.paused) {
+    audioElement10.play();
+    play10.innerHTML = "Pause";
+
+    audioElement.pause();
+    audioElement1.pause();
+    audioElement2.pause();
+    audioElement3.pause();
+    audioElement4.pause();
+    audioElement5.pause();
+    audioElement6.pause();
+    audioElement7.pause();
+    audioElement8.pause();
+    audioElement9.pause();
+    
+    play.innerHTML = "Play";
+    play1.innerHTML = "Play";
+    play2.innerHTML = "Play";
+    play3.innerHTML = "Play";
+    play4.innerHTML = "Play";
+    play5.innerHTML = "Play";
+    play6.innerHTML = "Play";
+    play7.innerHTML = "Play";
+    play8.innerHTML = "Play";
+    play9.innerHTML = "Play";
+  }
+   else {
+    audioElement10.pause();
+    play10.innerHTML = "Play";
+  }
+});
+
